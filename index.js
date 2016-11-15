7718a7aa2ad3e15e63c824ff4050e6691cbb1992
Roberto Aranda <ese.rober@gmail.com>
2016-11-15 23:24:59 +0100

    Removes ejs view engine as it is not required

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
+var path = require('path');
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
-app.use(express.static(__dirname + '/public'));
-
-app.set('views', __dirname + '/public');
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
+app.use(express.static(path.join(__dirname, 'public'));
 
 app.get('/', function (req, res) {
-  res.render('Hypotheke.html');
+  res.sendFile(path.join(__dirname, 'public', 'Hypotheke.html'));
 });
 
 app.listen(app.get('port'), function() {
